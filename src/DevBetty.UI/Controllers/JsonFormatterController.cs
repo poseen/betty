9da13b6c3f87a85767a87134f65fba0f2411433c
Viragh, Tibor <25325912+poseen@users.noreply.github.com>
2021-05-15 20:23:30 -0700

    Initial commit

---
+++ src/DevBetty.UI/Controllers/JsonFormatterController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace DevBetty.UI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class JsonFormatterController : ControllerBase
+    {
+        private readonly ILogger<JsonFormatterController> _logger;
+
+        public JsonFormatterController(ILogger<JsonFormatterController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            string body = null;
+
+            using (var reader = new System.IO.StreamReader(Request.Body))
+            {
+                body = await reader.ReadToEndAsync();
+            }
+
+            if (string.IsNullOrWhiteSpace(body))
+            {
+                return BadRequest();
+            }
+
+            string output = JsonConvert.SerializeObject(JsonConvert.DeserializeObject(body));
+            return Ok(output);
+        }
+    }
+}
---
+++ src/DevBetty.UI/Program.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace DevBetty.UI
+{
+    public class Program
+    {
+        // Dockerize - https://spin.atomicobject.com/2020/03/04/dockerizing-asp-net-react/
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
